bc3be448e3ae009405bae173524001a482372933
= <zainr7989@gmail.com>
2021-01-18 08:47:28 -0500

    Sync the camera and canvas aspect ratios.

---
+++ threejsTutorials/HelloCube/index.js
@@ -0,0 +1,74 @@
+//import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
+
+// do the thing 
+const main = () => {
+    // A: get the canvas element (what we'll be drawing upon)
+    const canvas = document.querySelector("#c");
+    // B: instaniate the renderer (to do the drawing)
+    const renderer = new THREE.WebGLRenderer({canvas});
+    // C: instantiate the camera
+    const fov = 75;  // fov = "field of view"
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    // move the camera to look down on the box
+    camera.position.z = 2;
+    // D: instantiate a scene - anything we want to draw gets added to it
+    const scene = new THREE.Scene();
+    // E: make the geometry for a box
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    /* F: 
+        * this function abstracts the steps for making a box, w/ different
+        * materials and geometries
+        */
+    const makeInstance = (geometry, color, x) => {
+        // set color on the material (can just use CSS)
+        const material = new THREE.MeshPhongMaterial({ color });
+        // instantiate the box
+        const cube = new THREE.Mesh(geometry, material);
+        // add it to the scene, and set its x position
+        scene.add(cube);
+        cube.position.x = x;
+        // end the function by returning the box
+        return cube;
+    }
+    // G: add 3 different cubes
+    const cubes = [
+        makeInstance(geometry, 0x44aa88, -2),
+        makeInstance(geometry, 0xaa8844, 0),
+        makeInstance(geometry, 0x8844aa, 2),
+    ];
+    // F: add a directional light
+    const color = 0xFFFFFF;  // just use white light for now
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+    // I: now render the scene!
+    const render = time => {
+        // convert time to seconds
+        time *= 0.001;  
+        // prevent the cubes being "stretched" - sync aspect ratios of canvas and camera
+        const canvas = renderer.domElement;  // this line works but isn't needed, b/c we only have one canvas
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+         // change the cube orientation
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+        });
+        // render the cube in one orientation
+        renderer.render(scene, camera);
+        // and see the cube again in rapid succession to create movement
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+}
+main();

8707146f3a94c74391494a0c8053b6a3d031373c
= <zainr7989@gmail.com>
2021-02-07 15:53:20 -0500

    Allow interactive animations.

--- threejsTutorials/HelloAnimations/index.js
+++ threejsTutorials/HelloAnimations/index.js
@@ -37,11 +37,13 @@ function main() {
   const manager = new THREE.LoadingManager();
   manager.onLoad = init;
 
+  // animate the progress bar moving left to right
   const progressbarElem = document.querySelector('#progressbar');
   manager.onProgress = (url, itemsLoaded, itemsTotal) => {
     progressbarElem.style.width = `${itemsLoaded / itemsTotal * 100 | 0}%`;
   };
 
+ // load the GLTF models
   const models = {
     pig:    { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Pig.gltf' },
     cow:    { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Cow.gltf' },
@@ -61,6 +63,7 @@ function main() {
     }
   }
 
+  // load in the animations of the models
   function prepModelsAndAnimations() {
     Object.values(models).forEach(model => {
       const animsByName = {};
@@ -71,7 +74,8 @@ function main() {
     });
   }
 
-  const mixers = [];
+  // store the animation data
+  const mixerInfos = [];
 
   function init() {
     // hide the loading bar
@@ -80,20 +84,51 @@ function main() {
 
     prepModelsAndAnimations();
 
+    // load in multiple models by "cloning"
     Object.values(models).forEach((model, ndx) => {
       const clonedScene = SkeletonUtils.clone(model.gltf.scene);
       const root = new THREE.Object3D();
       root.add(clonedScene);
       scene.add(root);
       root.position.x = (ndx - 3) * 3;
-
+      // add all the animations to the scene, for this model
       const mixer = new THREE.AnimationMixer(clonedScene);
-      const firstClip = Object.values(model.animations)[0];
-      const action = mixer.clipAction(firstClip);
-      action.play();
-      mixers.push(mixer);
+      const actions = Object.values(model.animations).map((clip) => {
+        return mixer.clipAction(clip);
+      });
+      // store the AnimationMixer, along with all its AnimationActions
+      const mixerInfo = {
+        mixer,
+        actions,
+        actionNdx: -1,
+      };
+      mixerInfos.push(mixerInfo);
+      playNextAction(mixerInfo);
     });
   }
+
+function playNextAction(mixerInfo) {
+  /* enable all but one animations for the mixer */
+  const {actions, actionNdx} = mixerInfo;
+  const nextActionNdx = (actionNdx + 1) % actions.length;
+  mixerInfo.actionNdx = nextActionNdx;
+  actions.forEach((action, ndx) => {
+    const enabled = ndx === nextActionNdx;
+    action.enabled = enabled;
+    if (enabled) {
+      action.play();
+    }
+  });
+}
+
+// allow the user to trigger the animations
+window.addEventListener('keydown', (e) => {
+  const mixerInfo = mixerInfos[e.key - 49];
+  if (!mixerInfo) {
+    return;
+  }
+  playNextAction(mixerInfo);
+});
 
   // render loop
   function resizeRendererToDisplaySize(renderer) {
@@ -119,7 +154,8 @@ function main() {
       camera.updateProjectionMatrix();
     }
 
-    for (const mixer of mixers) {
+    // update the animations
+    for (const {mixer} of mixerInfos) {
       mixer.update(deltaTime);
     }
 

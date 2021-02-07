3ba952a71bd827312b1a991663dffd36c7116f71
= <zainr7989@gmail.com>
2021-02-07 15:33:01 -0500

    Play the first animation.

--- threejsTutorials/HelloAnimations/index.js
+++ threejsTutorials/HelloAnimations/index.js
@@ -1,104 +1,134 @@
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js';
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
 import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/loaders/GLTFLoader.js';
 import {SkeletonUtils} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/utils/SkeletonUtils.js';
 
-/* The models come from the Farm Animals Pack, 
- * that was created by Quaternius: https://www.patreon.com/quaternius 
- */
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
+  const fov = 45;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 20, 40);
 
-/* load the GLTF models (for the animals) */
-const manager = new THREE.LoadingManager();
-manager.onLoad = init;
-const models = {
-  pig:    { url: 'GLTF/pig.gltf' },
-  cow:    { url: 'GLTF/cow.gltf' },
-  llama:  { url: 'GLTF/llama.gltf' },
-  pug:    { url: 'GLTF/pug.gltf' },
-  sheep:  { url: 'GLTF/sheep.gltf' },
-  zebra:  { url: 'GLTF/zebra.gltf' },
-  horse:  { url: 'GLTF/horse.gltf' },
-};
-{
-  const gltfLoader = new GLTFLoader(manager);
-  for (const model of Object.values(models)) {
-    gltfLoader.load(model.url, (gltf) => {
-      model.gltf = gltf;
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('white');
+
+  function addLight(...pos) {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(...pos);
+    scene.add(light);
+    scene.add(light.target);
+  }
+  addLight(5, 5, 2);
+  addLight(-5, 5, 5);
+
+  ///////////   NEW CONCEPTS  //////////////////////////////
+
+  const manager = new THREE.LoadingManager();
+  manager.onLoad = init;
+
+  const progressbarElem = document.querySelector('#progressbar');
+  manager.onProgress = (url, itemsLoaded, itemsTotal) => {
+    progressbarElem.style.width = `${itemsLoaded / itemsTotal * 100 | 0}%`;
+  };
+
+  const models = {
+    pig:    { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Pig.gltf' },
+    cow:    { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Cow.gltf' },
+    llama:  { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Llama.gltf' },
+    pug:    { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Pug.gltf' },
+    sheep:  { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Sheep.gltf' },
+    zebra:  { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Zebra.gltf' },
+    horse:  { url: 'https://threejsfundamentals.org/threejs/resources/models/animals/Horse.gltf' },
+    knight: { url: 'https://threejsfundamentals.org/threejs/resources/models/knight/KnightCharacter.gltf' },
+  };
+  {
+    const gltfLoader = new GLTFLoader(manager);
+    for (const model of Object.values(models)) {
+      gltfLoader.load(model.url, (gltf) => {
+        model.gltf = gltf;
+      });
+    }
+  }
+
+  function prepModelsAndAnimations() {
+    Object.values(models).forEach(model => {
+      const animsByName = {};
+      model.gltf.animations.forEach((clip) => {
+        animsByName[clip.name] = clip;
+      });
+      model.animations = animsByName;
     });
   }
-}
- 
-/* make the animations in the GLTF models easily accessible */
-function prepModelsAndAnimations() {
-  Object.values(models).forEach(model => {
-    const animsByName = {};
-    model.gltf.animations.forEach((clip) => {
-      animsByName[clip.name] = clip;
+
+  const mixers = [];
+
+  function init() {
+    // hide the loading bar
+    const loadingElem = document.querySelector('#loading');
+    loadingElem.style.display = 'none';
+
+    prepModelsAndAnimations();
+
+    Object.values(models).forEach((model, ndx) => {
+      const clonedScene = SkeletonUtils.clone(model.gltf.scene);
+      const root = new THREE.Object3D();
+      root.add(clonedScene);
+      scene.add(root);
+      root.position.x = (ndx - 3) * 3;
+
+      const mixer = new THREE.AnimationMixer(clonedScene);
+      const firstClip = Object.values(model.animations)[0];
+      const action = mixer.clipAction(firstClip);
+      action.play();
+      mixers.push(mixer);
     });
-    model.animations = animsByName;
-  });
-}
+  }
 
-// this array will hold our animations
-const mixers = [];
-
-/* main function */
-function init() {
-  // hide the loading bar
-  const loadingElem = document.querySelector('#loading');
-  loadingElem.style.display = 'none';
-
-  // load the models and their animations
-  prepModelsAndAnimations();
-
-  // clone the models just loaded
-  Object.values(models).forEach((model, ndx) => {
-    const clonedScene = SkeletonUtils.clone(model.gltf.scene);
-    // make a new Object3D to "parent" the clone (for positioning)
-    const root = new THREE.Object3D();
-    root.add(clonedScene);
-    scene.add(root);
-    root.position.x = (ndx - 3) * 3;
-
-  // play the animation of the first model
-  const firstClip = Object.values(model.animations)[0];
-  const action = mixer.clipAction(firstClip);
-  action.play();
-  // add the animations of our first model to the scene
-  const mixer = new THREE.AnimationMixer(clonedScene);
-  mixers.push(mixer);
-  });
-}
-
-const manager = new THREE.LoadingManager();
-manager.onLoad = init;
- 
-// show a progress bar go from 0-100% of the page width as the models load
-const progressbarElem = document.querySelector('#progressbar');
-manager.onProgress = (url, itemsLoaded, itemsTotal) => {
-  progressbarElem.style.width = `${itemsLoaded / itemsTotal * 100 | 0}%`;
-};
-
-/* render loop */
-let then = 0;
-function render(now) {
-  now *= 0.001;  // convert to seconds
-  // computing the time since the last frame
-  const deltaTime = now - then;
-  then = now;
- 
-  if (resizeRendererToDisplaySize(renderer)) {
+  // render loop
+  function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
   }
-  // update the animations as we render again
-  for (const mixer of mixers) {
-    mixer.update(deltaTime);
+
+  let then = 0;
+  function render(now) {
+    now *= 0.001;  // convert to seconds
+    const deltaTime = now - then;
+    then = now;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    for (const mixer of mixers) {
+      mixer.update(deltaTime);
+    }
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
   }
- 
-  renderer.render(scene, camera);
- 
+
   requestAnimationFrame(render);
 }
+
+main();

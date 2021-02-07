65c787936f8ac4e52bf1e6fa5e4b9ee404f9b98e
= <zainr7989@gmail.com>
2021-02-07 14:54:49 -0500

    Add animations to the scene.

--- threejsTutorials/HelloAnimations/index.js
+++ threejsTutorials/HelloAnimations/index.js
@@ -1,3 +1,8 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/loaders/GLTFLoader.js';
+import {SkeletonUtils} from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/utils/SkeletonUtils.js';
+
 /* The models come from the Farm Animals Pack, 
  * that was created by Quaternius: https://www.patreon.com/quaternius 
  */
@@ -24,11 +29,46 @@ const models = {
   }
 }
  
+/* make the animations in the GLTF models easily accessible */
+function prepModelsAndAnimations() {
+  Object.values(models).forEach(model => {
+    const animsByName = {};
+    model.gltf.animations.forEach((clip) => {
+      animsByName[clip.name] = clip;
+    });
+    model.animations = animsByName;
+  });
+}
+
+// this array will hold our animations
+const mixers = [];
+
+/* main function */
 function init() {
-  // TODO: main function
   // hide the loading bar
   const loadingElem = document.querySelector('#loading');
   loadingElem.style.display = 'none';
+
+  // load the models and their animations
+  prepModelsAndAnimations();
+
+  // clone the models just loaded
+  Object.values(models).forEach((model, ndx) => {
+    const clonedScene = SkeletonUtils.clone(model.gltf.scene);
+    // make a new Object3D to "parent" the clone (for positioning)
+    const root = new THREE.Object3D();
+    root.add(clonedScene);
+    scene.add(root);
+    root.position.x = (ndx - 3) * 3;
+
+  // play the animation of the first model
+  const firstClip = Object.values(model.animations)[0];
+  const action = mixer.clipAction(firstClip);
+  action.play();
+  // add the animations of our first model to the scene
+  const mixer = new THREE.AnimationMixer(clonedScene);
+  mixers.push(mixer);
+  });
 }
 
 const manager = new THREE.LoadingManager();
@@ -39,3 +79,26 @@ const progressbarElem = document.querySelector('#progressbar');
 manager.onProgress = (url, itemsLoaded, itemsTotal) => {
   progressbarElem.style.width = `${itemsLoaded / itemsTotal * 100 | 0}%`;
 };
+
+/* render loop */
+let then = 0;
+function render(now) {
+  now *= 0.001;  // convert to seconds
+  // computing the time since the last frame
+  const deltaTime = now - then;
+  then = now;
+ 
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  // update the animations as we render again
+  for (const mixer of mixers) {
+    mixer.update(deltaTime);
+  }
+ 
+  renderer.render(scene, camera);
+ 
+  requestAnimationFrame(render);
+}

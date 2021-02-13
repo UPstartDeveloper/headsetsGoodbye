6815a125ebec6b78a9ffe83a292e16d150f3a3e8
= <zainr7989@gmail.com>
2021-02-13 11:56:40 -0500

    Fix error in new cubes not showing.

--- threejsTutorials/HelloCube/index.js
+++ threejsTutorials/HelloCube/index.js
@@ -19,56 +19,10 @@ Split(['#view', '#controls'], {  // eslint-disable-line new-cap
     };
   },
 });
-// resize the canvas to prevent poor resolution
-const resizeRendererToDisplaySize = renderer => {
-    // get the current dimensions of the canvas (on the HTML document)
-    const view = document.querySelector("#view");
-    // calculate the canvas' pixel area, account for HD-DPI pixel ratios
-    const pixelRatio = window.devicePixelRatio;
-    const width = view.clientWidth * pixelRatio | 0;
-    const height = view.clientHeight * pixelRatio | 0;
-    // determine if the canvas' (pixel area) needs to be resized
-    const needResize = canvas.width !== width || canvas.height !== height;
-    // resize the canvas to the same area as currently seen on the screen
-    if (needResize) {
-        renderer.setSize(width, height, false);
-    }
-    return needResize;
-}
 
-// generating a single random number in a specified range
-const rand = (min, max) => {
-  // returns a random number between max and min
-  if (max === undefined) {
-    max = min;
-    min = 0;
-  }
-  return min + (max - min) * Math.random();
-}
- 
-const randomColor = () => {
-  // defines the hue and saturation for the color randomly
-  return `hsl(${rand(360) | 0}, ${rand(50, 100) | 0}%, 50%)`;
-}
- 
-/* Generating 100 cubes with random colors, positions, orientations,
- * and scales. 
- */
-const numObjects = 100;
-for (let i = 0; i < numObjects; ++i) {
-  const material = new THREE.MeshPhongMaterial({
-    color: randomColor(),
-  });
- 
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
- 
-  cube.position.set(rand(-20, 20), rand(-20, 20), rand(-20, 20));
-  cube.rotation.set(rand(Math.PI), rand(Math.PI), 0);
-  cube.scale.set(rand(3, 6), rand(3, 6), rand(3, 6));
-}
+
 // rendering the scene
-const renderCubes = () => {
+const main = () => {
     // A: get the canvas element (what we'll be drawing upon)
     const canvas = document.querySelector("#c");
     // B: instaniate the renderer (to do the drawing)
@@ -94,27 +48,52 @@ const renderCubes = () => {
     const boxHeight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-    /* F: 
-        * this function abstracts the steps for making a box, w/ different
-        * materials and geometries
-        */
-    const makeInstance = (geometry, color, x) => {
-        // set color on the material (can just use CSS)
-        const material = new THREE.MeshPhongMaterial({ color });
-        // instantiate the box
-        const cube = new THREE.Mesh(geometry, material);
-        // add it to the scene, and set its x position
-        scene.add(cube);
-        cube.position.x = x;
-        // end the function by returning the box
-        return cube;
+    // F: Adding 100 random cubes to the scene
+    // generating a single random number in a specified range
+    const rand = (min, max) => {
+      // returns a random number between max and min
+      if (max === undefined) {
+        max = min;
+        min = 0;
+      }
+      return min + (max - min) * Math.random();
     }
-    // G: add 3 different cubes
-    const cubes = [
-        makeInstance(geometry, 0x44aa88, -2),
-        makeInstance(geometry, 0xaa8844, 0),
-        makeInstance(geometry, 0x8844aa, 2),
-    ];
+    // defines the hue and saturation for the color randomly
+    const randomColor = () => {
+      return `hsl(${rand(360) | 0}, ${rand(50, 100) | 0}%, 50%)`;
+    }
+    /* Generating 100 cubes with random colors, positions, orientations,
+    * and scales. 
+    */
+    const numObjects = 100;
+    for (let i = 0; i < numObjects; ++i) {
+      const material = new THREE.MeshPhongMaterial({
+        color: randomColor(),
+      });
+    
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+    
+      cube.position.set(rand(-20, 20), rand(-20, 20), rand(-20, 20));
+      cube.rotation.set(rand(Math.PI), rand(Math.PI), 0);
+      cube.scale.set(rand(3, 6), rand(3, 6), rand(3, 6));
+    }
+    // resize the canvas to prevent poor resolution
+    const resizeRendererToDisplaySize = renderer => {
+        // get the current dimensions of the canvas (on the HTML document)
+        const view = document.querySelector("#view");
+        // calculate the canvas' pixel area, account for HD-DPI pixel ratios
+        const pixelRatio = window.devicePixelRatio;
+        const width = view.clientWidth * pixelRatio | 0;
+        const height = view.clientHeight * pixelRatio | 0;
+        // determine if the canvas' (pixel area) needs to be resized
+        const needResize = canvas.width !== width || canvas.height !== height;
+        // resize the canvas to the same area as currently seen on the screen
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
     // F: add a directional light
     const color = 0xFFFFFF;  // just use white light for now
     const intensity = 1;
@@ -134,13 +113,6 @@ const renderCubes = () => {
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
             camera.updateProjectionMatrix();
         } 
-         // change the cube orientation
-        cubes.forEach((cube, ndx) => {
-            const speed = 1 + ndx * .1;
-            const rot = time * speed;
-            cube.rotation.x = rot;
-            cube.rotation.y = rot;
-        });
         // spin the camera pole
         cameraPole.rotation.y = time * .1;
         // render the cubes in one orientation
@@ -150,4 +122,4 @@ const renderCubes = () => {
     }
     requestAnimationFrame(render);
 }
-renderCubes();
+main();

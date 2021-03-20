0d6a68f119fa89ebe1b07d0e293040d0143d6c7a
= <zainr7989@gmail.com>
2021-03-20 15:43:04 -0400

    Improve 'smoothness' of face tracking.

--- handsfreeDemos/faceAndHands/environments/cubes.js
+++ handsfreeDemos/faceAndHands/environments/cubes.js
@@ -80,12 +80,7 @@ const addSolidGeometry = (x, y, geometry) => {
   addObject(x, y, mesh);
 }
 
-/* rendering the scene */
-export const renderCubes = (trackingFunc, handsfreeTracker) => {
-    // A: get the canvas element (what we'll be drawing upon)
-    const canvas = document.querySelector("#c");
-    // B: instaniate the renderer (to do the drawing)
-    const renderer = new THREE.WebGLRenderer({canvas});
+export const makeCamera = () => {
     // C: instantiate the camera
     const fov = 40;  // fov = "field of view"
     const aspect = 2;  // the canvas default
@@ -94,6 +89,15 @@ export const renderCubes = (trackingFunc, handsfreeTracker) => {
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     // move the camera to look down on the box
     camera.position.z = 8;
+    return camera;
+}
+
+/* rendering the scene */
+export const renderCubes = (camera) => {
+    // A: get the canvas element (what we'll be drawing upon)
+    const canvas = document.querySelector("#c");
+    // B: instaniate the renderer (to do the drawing)
+    const renderer = new THREE.WebGLRenderer({canvas});
     // D: instantiate a scene - anything we want to draw gets added to it
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);  // black
@@ -151,8 +155,6 @@ export const renderCubes = (trackingFunc, handsfreeTracker) => {
         renderer.render(scene, camera);
         // and see the cube again in rapid succession to create movement
         requestAnimationFrame(render);
-        // let Handsfree js also manipulate the camera
-        trackingFunc(handsfreeTracker, camera);
     }
     // render the scene
     requestAnimationFrame(render);

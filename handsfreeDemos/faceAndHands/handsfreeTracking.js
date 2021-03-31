7edf165dfacb688fd51dd4c2a749fb9aaf2d2c53
= <zainr7989@gmail.com>
2021-03-31 18:59:04 -0400

    Init plugin for fistGrab gesture.

--- handsfreeDemos/faceAndHands/handsfreeTracking.js
+++ handsfreeDemos/faceAndHands/handsfreeTracking.js
@@ -52,10 +52,11 @@ export function startHandsAndThree() {
         trackFace(window.handsfree, camera);
         // C: set up the Three.js environment, 
         renderCubes(camera);
-        // add hand tracking (logging to verify)
-        window.handsfree.update({hands: true})
-        console.log("Added weboji:" + window.handsfree.model.weboji);
-        console.log("Added model:" + window.handsfree.model.handpose);
+        // D: add hand tracking
+        window.handsfree.update({hands: true});
+        trackHand(window.handsfree);
+        // console.log("Added handpose model:" + window.handsfree.model.handpose);
+        // console.log("Added weboji:" + window.handsfree.model.weboji);
     })
 }
 
@@ -86,6 +87,92 @@ const trackFace = (handsfree, camera) => {
     })
     // console.log("position new " + pos.x + ", " + pos.y + ", " + pos.z)
 
+    })
+}
+
+const trackHand = handsfree => {
+    handsfree.use('pinchClick', {
+        config: {
+            // Number of pixels that the finger/thumb tips must be within to trigger a click
+            // Too low a value will not work due to errors
+            pinchDistance: 40,
+
+            // Number of frames after a release is detected to still consider as a drag (helps with tracking errors)
+            numErrorFrames: 5
+        },
+
+        // Are the fingers pinched?
+        pinched: false,
+        // Have the fingers been released?
+        released: false,
+        // Whether the finger/thumb are considered pinched
+        pinchThresholdMet: false,
+        // Number of frames after a click is NOT detected to actually release the click (helps with errors)
+        numErrorFrames: 5,
+
+        /**
+         * Detect click state and trigger a real click event
+         * - This is the only method required for a plugin to work
+         */
+        onFrame({ hand }) {
+            // Bail if no detection
+            if (!hand || !hand.annotations) return
+
+            // Detect if the thumb and indexFinger are pinched together
+            const a = hand.annotations.indexFinger[3][0] - hand.annotations.thumb[3][0]
+            const b = hand.annotations.indexFinger[3][1] - hand.annotations.thumb[3][1]
+            const c = Math.sqrt(a*a + b*b)
+            this.pinchThresholdMet = c < this.config.pinchDistance
+
+            // Count number of frames since last pinch to help with errors
+            if (this.pinchThresholdMet) {
+                this.numErrorFrames = 0
+            } else {
+                ++this.numErrorFrames
+            }
+
+            // Simulate a mousemove (moving the block)
+            if (this.pinched && this.numErrorFrames < this.config.numErrorFrames) {
+                hand.pointer.state = 'mousemove'
+            }
+
+            // Simulate a mousedown (selecting a block)
+            if (this.pinchThresholdMet && !this.pinched) {
+                this.pinched = true
+                this.released = false
+                document.body.classList.add('handsfree-clicked')
+                hand.pointer.state = 'mousedown'
+            }
+
+            // Simulate a mouseup (unpinch)
+            if (!this.pinchThresholdMet && !this.released && this.numErrorFrames < this.config.numErrorFrames) {
+                this.pinched = false
+                this.released = true
+                document.body.classList.remove('handsfree-clicked')
+                hand.pointer.state = 'mouseup'
+            }
+
+            // Dispatch events
+            window.renderer && hand.pointer.state && this.dispatchEvent(hand)
+        },
+
+        /**
+         * The actual click method, this is what gets throttled
+         */
+        dispatchEvent(hand) {
+            const $el = document.elementFromPoint(hand.pointer.x, hand.pointer.y);
+            if ($el) {
+                window.renderer.domElement.dispatchEvent(
+                    new MouseEvent(hand.pointer.state, {
+                        bubbles: true,
+                        cancelable: true,
+                        clientX: hand.pointer.x,
+                        clientY: hand.pointer.y
+                    })
+                )
+                hand.pointer.$target = $el
+            }
+        }
     })
 }
 

6d6d3da7e73bdb87dc898dbf3a2f275e9d367aa1
= <zainr7989@gmail.com>
2021-04-11 14:06:33 -0400

    List face-api as dependency.

--- expressionTrackingDemos/robot/expressionTracker.js
+++ expressionTrackingDemos/robot/expressionTracker.js
@@ -3,7 +3,7 @@
  * https://www.opensourceforu.com/2020/06/building-a-facial-expression-recognition-app-using-tensorflow-js/
  */
 
-import * as faceapi from "https://cdn.jsdelivr.net/gh/justadudewhohacks/face-api.js/dist/face-api.min.js";
+import "https://cdn.jsdelivr.net/gh/justadudewhohacks/face-api.js/dist/face-api.js";
 
 // A: Laod in the neural net for identifying and analyzing faces
 Promise.all([
@@ -23,19 +23,13 @@ function startVideo() {
     );
 }
 
-video.addEventListener('play', () => {
-    // Continuously animate the robot w/ expressions found on the user
-    // const canvas = faceapi.createCanvasFromMedia(video)
-    // document.body.append(canvas)
-    // const displaySize = { width: video.width, height: video.height }
-    // faceapi.matchDimensions(canvas, displaySize)
-    setInterval(async() => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
-        console.log(detections);
-        // const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        // faceapi.draw.drawDetections(canvas, resizedDetections)
-        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    }, 100)
-})
+
+setInterval(async() => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
+    console.log(detections);
+    // const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    // faceapi.draw.drawDetections(canvas, resizedDetections)
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+}, 100);
